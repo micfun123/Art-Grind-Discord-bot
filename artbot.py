8c04beb3fe4f87ec22499eb318adfab640bd30be
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-04-20 14:23:31 +0400

    twitter

--- artbot.py
+++ artbot.py
@@ -1,5 +1,6 @@
 from pydoc import describe
 from string import whitespace
+from unicodedata import name
 import discord
 import random
 import os
@@ -157,6 +158,14 @@ async def Code(ctx):
     em.add_field(name="Link :", value="https://github.com/micfun123/Art-Grind-Discord-bot")
     await ctx.respond(embed=em)
     
+
+@client.slash_command()
+async def Code(ctx):
+    em = discord.Embed(title="Oh My maker", description="I was made by the worlds by lord <@481377376475938826>. ", color=0x00ff00)
+    em.add_field(name="Github :", value="<https://github.com/micfun123/>")
+    em.add_field(name="twitter :", value="<https://twitter.com/Michaelrbparker>")
+    em.footer(name = "Go say hi to him")
+    await ctx.respond(embed=em)
 
 @client.slash_command()
 async def duelidea(ctx):
