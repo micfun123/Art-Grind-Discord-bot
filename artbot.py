a3ab416a66c242ddb891978007fa20067f156eb1
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-05-01 18:21:29 +0400

    fixed

--- artbot.py
+++ artbot.py
@@ -158,11 +158,12 @@ async def weekly_challenge():
         randoms = random.choice(allmes)
         chennel2 = client.get_channel(915305657803108362)
         em = discord.Embed(title=f"weekly challenge",color=0x00ff00)
-        em.description = "<@&856677753125208081>\n Its your favorite time of the week again!\n"
+        em.description = "Its your favorite time of the week again!\n"
         em.add_field(name="Challenge :", value=randoms.content)
         msg = await channel.fetch_message(randoms.id)
         print(msg.content)
         await chennel2.send(embed=em)
+        await chennel2.send(f'<@&856677753125208081>')
         await msg.delete()
 
 @client.command()
