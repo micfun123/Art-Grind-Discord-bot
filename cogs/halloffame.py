82a1365deaf449df7c6350577eae0e4a5b8957e5
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-03-18 19:22:24 +0000

    art

--- cogs/halloffame.py
+++ cogs/halloffame.py
@@ -23,7 +23,7 @@ class halloffame(commands.Cog):
         message = payload.message_id
         channel = payload.channel_id
         guild = payload.guild_id
-        tosend = 991740480398311544
+        tosend = 1086347720463220786
         content = await self.client.get_channel(channel).fetch_message(message)
         for reaction in content.reactions:
             if reaction.emoji == "❤️":
@@ -31,7 +31,7 @@ class halloffame(commands.Cog):
                     if message in data:
                         pass
                     else:
-                        if channel == 1086347720463220786:
+                        if channel == 1086410514348904529:
                             await self.client.get_channel(tosend).send(f"|| {content.attachments[0]} ||")
                             madeby = content.author.name
                             await self.client.get_channel(tosend).send(f"Made by: {madeby} **warning this image is has gore**")
@@ -47,6 +47,34 @@ class halloffame(commands.Cog):
                                 json.dump(data, wfile)
                     
                     break
+
+    @commands.command()
+    @commands.is_owner()
+    async def check_heart_all(self, ctx):
+        tosend = 1086347720463220786
+        for channel in ctx.guild.text_channels:
+            for message in channel.history(limit=300):
+                for reaction in message.reactions:
+                    if reaction.emoji == "❤️":
+                        if reaction.count == 10:
+                            if message.id in data:
+                                pass
+                            else:
+                                if channel.id == 1086347720463220786:
+                                    await self.client.get_channel(tosend).send(f"|| {message.attachments[0]} ||")
+                                    madeby = message.author.name
+                                    await self.client.get_channel(tosend).send(f"Made by: {madeby} **warning this image is has gore**")
+                                    data.append(message.id)
+                                    with open ("posts.json", "w") as wfile:
+                                        json.dump(data, wfile)
+                                else:
+                                    await self.client.get_channel(tosend).send(f"{message.attachments[0]}")
+                                    madeby = message.author.name
+                                    await self.client.get_channel(tosend).send(f"Made by: {madeby}")
+                                    data.append(message.id)
+                                    with open ("posts.json", "w") as wfile:
+                                        json.dump(data, wfile)
+                                break
 
 
                     
